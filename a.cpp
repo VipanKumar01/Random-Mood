1cd6c6bea52364573a7ae80aa0bb8967e4b879a2
Vipan Kumar <108624219+VipanKumar01@users.noreply.github.com>
2022-10-20 09:33:40 -0700

    Create a.cpp

---
+++ a.cpp
@@ -0,0 +1,26 @@
+// Hii Hii
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int n;
+  cout<<"Enter No. To Print Pattern : ";
+  cin>>n;
+  cout<<"Hello Guys!";
+  
+  for(int i=0;i<=n;i++)
+  {
+    for(int space=0;space<n-i;space++)
+      cout<<" ";
+    for(int j=0;j<i;j++)
+      cout<<"*";
+    
+    cout<<endl;
+  }
+  
+}
+
+
+//    --HappyCode--
